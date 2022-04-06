00c6ee351ee652846d6350bd4348a080c69678fa
henk jurriens <henk.jurriens@gmail.com>
2022-04-06 22:43:36 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/r
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
 import Menu from './components/Menu';
-import Page from './pages/Page';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
